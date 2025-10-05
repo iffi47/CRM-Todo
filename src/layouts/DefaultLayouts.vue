51394bcb3226085e07559e5323be284db0f69f8f
IFTIKHAR AHMAD <66529774+iffi47@users.noreply.github.com>
2025-10-05 16:18:21 +0500

    fix: fix Basic Layout Structure of App

--- src/layouts/DefaultLayouts.vue
+++ src/layouts/DefaultLayouts.vue
@@ -1,20 +1,38 @@
 <template>
-  <!-- App.vue -->
-  <!-- <router-view :to="/auth"></router-view> -->
-  <v-app id="app">
-    <div>
-      <RouterView></RouterView>
+  <div v-show="state">
+      <!--App bar-->
+      <AppBar v-if="state"></AppBar>
+      <v-main class="bg-grey-lighten-3">
+        <v-container>
+          <v-row>
+            <v-col class="ma-4" cols="2">
+              <!--Side Nav-->
+              <SideNav v-if="state" :links="links"></SideNav>
+            </v-col>
+            <v-col>
+              <!--Main Content-->
+              <v-sheet min-height="70vh" rounded="lg">
+                <!--  -->
+                <RouterView></RouterView>
+                <!-- <component v-for="(component, index) in links" :key="index" :is="component.component"></component> -->
+              </v-sheet>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
     </div>
-  </v-app>
 </template>
 <script setup>
+
 import { computed } from "vue"
 import { useStore } from "vuex"
 import { useRouter } from "vue-router";
 import { onMounted, ref } from "vue"
+
 const router = useRouter()
 const store = useStore()
-
+import SideNav from '../components/SideNav.vue';
+import AppBar from '../components/AppBar.vue';
 const state = computed(() => store.state.user)
 onMounted(() => {
   const user= JSON.parse(localStorage.getItem("user")) 
@@ -46,5 +64,5 @@ const Userlinks = ref([
 ])
 
 
+
 </script>
-<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,52 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import DefaultLayouts from '../layouts/DefaultLayouts.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'Home',
-      component: () => import('../views/HomePage.vue')
-    },
-    {
       path: '/login',
       name: 'Login',
       component: () => import('../views/LoginPage.vue')
     },
+
     {
-      path: '/users',
-      name: 'Users',
-      component: () => import('../views/Admin/ListUser.vue')
-    },
-    {
-      path: '/add-user',
-      name: 'AddUser',
-      component: () => import('../views/Admin/AddUser.vue')
-    },
-    {
-      path: '/edit-user/:id',
-      name: 'EditUser',
-      component: () => import('../views/Admin/AddUser.vue')
-    },
-    {
-      path: '/tasks',
-      name: 'Tasks',
-      component: () => import('../views/User/ListTasks.vue')
-    },
-    {
-      path: '/edit-task/:id/:userId',
-      name: 'EditTask',
-      component: () => import('../views/User/EditTask.vue')
+      name: 'DefaultLayouts',
+      path: '/',
+      component: DefaultLayouts,
+      children: [
+        {
+          path: '',
+          name: 'Home',
+          component: () => import('../views/HomePage.vue')
+        },
+
+        {
+          path: '/users',
+          name: 'Users',
+          component: () => import('../views/Admin/ListUser.vue')
+        },
+        {
+          path: '/add-user',
+          name: 'AddUser',
+          component: () => import('../views/Admin/AddUser.vue')
+        },
+        {
+          path: '/edit-user/:id',
+          name: 'EditUser',
+          component: () => import('../views/Admin/AddUser.vue')
+        },
+        {
+          path: '/tasks',
+          name: 'Tasks',
+          component: () => import('../views/User/ListTasks.vue')
+        },
+        {
+          path: '/edit-task/:id/:userId',
+          name: 'EditTask',
+          component: () => import('../views/User/EditTask.vue')
+        }
+      ]
     }
   ]
 })
